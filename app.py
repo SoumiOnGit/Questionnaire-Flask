5ed0352054609ed12a82d106233e0e5fc1d00853
gokul-1998 <82328858+gokul-1998@users.noreply.github.com>
2023-09-03 20:41:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
         'question': 'Sample question here...',
         'options': ['Option 1...', 'Option 2...', 'Option 3...']
     }
-    return render_template('question.html', question_data=question_data)
+    return render_template('index.html', question_data=question_data)
 
 if __name__ == '__main__':
     app.run()
